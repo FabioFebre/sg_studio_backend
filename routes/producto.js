055656bff6345a1395129a318a3db602e272a65e
FabioFebre <fabio.febre@tecsup.edu.pe>
2025-05-27 09:43:34 -0500

    27/5

--- routes/producto.js
+++ routes/producto.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { Producto, Categoria } = require('../models');
 const multer = require('multer');
+const path = require('path');
 
 
 
