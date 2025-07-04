2e9eac4e4b59038c3fcc542351e560d5d66cae8c
FabioFebre <fabio.febre@tecsup.edu.pe>
2025-07-03 20:07:02 -0500

    03/09

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 // index.js
 const express = require('express');
 const app = express();
-app.use(express.json());
+
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ extended: true, limit: '25mb' }));
+
 const db = require('./models');
 const cors = require('cors');
+
+
 app.use(cors({
   origin: ['https://www.sgstudio.shop', 'http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true 
 }));
+
 require('dotenv').config();
 console.log('Base de datos actual:', db.sequelize.config.database);
 const path = require('path');
